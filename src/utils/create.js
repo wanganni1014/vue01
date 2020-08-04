ac34f2e7a8969f8af386b8037b639eec020376ae
annie <wanganni@zhilunkeji.com>
2020-08-04 17:16:26 +0800

    增加理解注释

--- src/utils/create.js
+++ src/utils/create.js
@@ -19,7 +19,7 @@ function create(component, props) {
     // 创建 Profile 实例，并挂载到一个元素上。
     // new Profile().$mount('#mount-point')
     
-    // 可以看到，extend 创建的是 Vue 构造器，而不是我们平时常写的组件实例，所以不可以通过 new Vue({ components: testExtend }) 来直接使用，
+    // 可以看到，extend 创建的是 Vue 构造器（直接得到根实例，类型为VueComponent），而不是我们平时常写的组件实例，所以不可以通过 new Vue({ components: testExtend }) 来直接使用，
     // 需要通过 new Profile().$mount('#mount-point') 来挂载到指定的元素上。
     // 我们可以通过 $el 属性来访问 extendComponent 组件实例
 
@@ -38,16 +38,18 @@ function create(component, props) {
 
 
 
-    // 创建一个Vue实例
+    // 创建一个Vue实例，组件实例 $root
     // const vm = new Vue({
         // render函数将传入数组配置对象转化为虚拟dom
     //     render: h => h(component, { props })
+    // 将组件实例挂载在根组件上，通常为APP
     // }).$mount();  //执行挂载函数，但为指定挂载目标。表示只执行初始化工作。（因为挂载是覆盖线动作，所以这里不直接挂载在body上）
 
     // 将生成的dom元素追加至body
     // document.body.appendChild(vm.$el)
 
     // 给组件实例添加销毁方法
+    // 因为vue是单根的，所以可以通过vm.$children[0]获取根组建的实例, 类型为VueComponent
     // const comp = vm.$children[0];
     // comp.remove = function() {
     //     document.body.removeChild(vm.$el);
